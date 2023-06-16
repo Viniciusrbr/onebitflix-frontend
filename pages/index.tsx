a13903fb49418e406959ea5b797ee1297330c99d
Viniciusrbr <viniciusgoes159@gmail.com>
2023-06-16 20:39:13 -0300

    Criação do header da página homeNoAuth - Header Não autenticado

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "../styles/homeNoAuth.module.scss"
+import HeaderNoAuth from "@/src/components/homeNoAuth/headerNoAuth";
 
 const HomeNotAuth = function () {
   return (
@@ -8,10 +9,10 @@ const HomeNotAuth = function () {
         <title>Onebitflix</title>
         <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
         <meta property="og:title" content="Onebitflix" key="title" />
-        <meta name="description" content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil."/>
+        <meta name="description" content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil." />
       </Head>
       <main>
-
+        <HeaderNoAuth />
       </main>
     </>
   );
