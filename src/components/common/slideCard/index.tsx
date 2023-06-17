111a3a075ff9ce53eccadcb9ba69f7956b74a0da
Viniciusrbr <viniciusgoes159@gmail.com>
2023-06-17 20:37:00 -0300

    Criação dos slides, componente pai e filho. Conexão com o backend, inicio do visual dos slides

---
+++ src/components/common/slideCard/index.tsx
@@ -0,0 +1,21 @@
+import styles from './styles.module.scss'
+import { CourseType } from '@/src/services/courseService';
+
+interface props {
+    course: CourseType
+}
+
+const SlideCard = function ({ course }: props) {
+    return (
+        <>
+            <div className={styles.slide}>
+                <img className={styles.slideImg} src={`${process.env.NEXT_PUBLIC_BASEURL}/${course.thumbnailUrl}`} 
+                alt={course.name} />
+                <p className={styles.slideTitle}>{course.name}</p>
+                <p className={styles.slideDescription}>{course.synopsis}</p>
+            </div>
+        </>
+    )
+}
+
+export default SlideCard;
